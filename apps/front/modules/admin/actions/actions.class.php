f2415e77b6f0a323390a9d338583fb6f90c1dcd3
Elias De la Torre <elias.delatorre@axai.com.mx>
2009-02-16 14:39:50 -0600

    Generé el módulo de administración de CEREBRO

---
+++ apps/front/modules/admin/actions/actions.class.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * admin actions.
+ *
+ * @package    cerebro
+ * @subpackage admin
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 12479 2008-10-31 10:54:40Z fabien $
+ */
+class adminActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+   $q = Doctrine_Query::create()
+     ->from('Producto p');
+//     ->where('p.activa=true');
+     $this->producto_list = $q->execute();
+   $q = Doctrine_Query::create()
+     ->from('Plaza p');
+//     ->where('p.activa=true');
+     $this->plaza_list = $q->execute();
+   $q = Doctrine_Query::create()
+     ->from('Specialty s');
+//     ->where('s.activa=true');
+     $this->specialty_list = $q->execute();
+   $q = Doctrine_Query::create()
+     ->from('Impresor i');
+//     ->where('i.activa=true');
+     $this->impresor_list = $q->execute();
+   $q = Doctrine_Query::create()
+     ->from('Mensajeria m');
+//     ->where('t.activa=true');
+     $this->mensajeria_list = $q->execute();
+   $q = Doctrine_Query::create()
+     ->from('Tipo t');
+//     ->where('t.activa=true');
+     $this->tipo_list = $q->execute();
+  }
+}
---
+++ apps/front/modules/admin/templates/indexSuccess.php
@@ -0,0 +1,62 @@
+<h1>Administración del sistema</h1>
+
+<table>
+    <thead><tr><th scope="col"><a href="<?php echo url_for('plaza') ?>">Plazas</a></th></tr></thead>
+    <tbody>
+    <?php foreach($plaza_list as $plaza): ?>
+    <tr><td><?php echo $plaza->getnombre()?></td></tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
+
+
+<table>
+    <thead><tr><th scope="col"><a href="<?php echo url_for('specialty') ?>">Specialties</a></th></tr></thead>
+    <tbody>
+    <?php foreach($specialty_list as $specialty): ?>
+    <tr><td><?php echo $specialty->getnombre()?></td></tr>
+    <?php endforeach; ?>
+</tbody>
+</table>
+
+<table>
+    <thead><tr><th scope="col"><a href="<?php echo url_for('impresor') ?>">Impresores</a></th></tr></thead>
+    <tbody>
+    <?php foreach($impresor_list as $impresor): ?>
+    <tr><td><?php echo $impresor->getnombre()?></td></tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
+
+
+<table>
+    <thead><tr><th scope="col"><a href="<?php echo url_for('mensajeria') ?>">Mensajerias</a>
+</th></tr></thead>
+    <tbody>
+    <?php foreach($mensajeria_list as $mensajeria): ?>
+    <tr><td><?php echo $mensajeria->getnombre()?></td></tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
+
+<table>
+    <thead><tr><th scope="col"><a href="<?php echo url_for('producto') ?>">Productos</a>
+</th></tr></thead>
+    <tbody>
+    <?php foreach($producto_list as $producto): ?>
+    <tr><td><?php echo $producto->getnombre()?></td></tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
+
+<table>
+<thead><tr><th scope="col"><a href="<?php echo url_for('tipo') ?>">Tipos</a></th></tr></thead>
+    <tbody>
+    <?php foreach($tipo_list as $tipo): ?>
+    <tr><td><?php echo $tipo->getnombre()?></td></tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
+
+
+
