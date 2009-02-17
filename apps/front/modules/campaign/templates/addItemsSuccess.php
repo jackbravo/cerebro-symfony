c56d8c170043f8fe06ffb614199459fc9dec1044
Elias De la Torre <elias.delatorre@axai.com.mx>
2009-02-16 19:47:09 -0600

    Agregar los headers a la tabla donde irán los items

--- apps/front/modules/campaign/templates/addItemsSuccess.php
+++ apps/front/modules/campaign/templates/addItemsSuccess.php
@@ -1,3 +1,24 @@
+<?php use_helper('Object') ?>
 <?php echo $campaign->nombre ?>
 <form action="<?php echo url_for('campaign_createItems', $campaign) ?>" method="post">
+<table>
+  <thead>
+    <tr>
+      <th>Plaza</th>
+      <th>No. de Items</th>
+      <th>Categoría</th>
+      <th>Tipo</th>
+      <th>Responsable</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  </tbody>
+</table>
 </form>
