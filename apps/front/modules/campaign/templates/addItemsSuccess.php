1b65f3834128607dd483151e1e956219d1840135
Elias De la Torre <elias.delatorre@axai.com.mx>
2009-02-17 12:38:54 -0600

    Agregar los campos: número de item, plazas, categoría, reponsable y tipos

--- apps/front/modules/campaign/templates/addItemsSuccess.php
+++ apps/front/modules/campaign/templates/addItemsSuccess.php
@@ -1,5 +1,5 @@
 <?php use_helper('Object') ?>
-<?php echo $campaign->nombre ?>
+<h2>Agregar items a: <?php echo $campaign->nombre ?></h2>
 <form action="<?php echo url_for('campaign_createItems', $campaign) ?>" method="post">
 <table>
   <thead>
@@ -13,11 +13,23 @@
   </thead>
   <tbody>
     <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
+      <td><?php echo select_tag('plaza_id[]', objects_for_select(
+                  Doctrine::getTable('Plaza')->findAll(),
+                  'getId', '__toString', '')
+                ) ?></td>
+      <td><input type="text" name="no_items[]" size="10" /></td>
+      <td><?php echo select_tag('categoria_id[]', objects_for_select(
+                  Doctrine::getTable('CategoriaItem')->findAll(),
+                  'getId', '__toString', '')
+                ) ?></td>
+      <td><?php echo select_tag('tipo_id[]', objects_for_select(
+                  Doctrine::getTable('Tipo')->findAll(),
+                  'getId', '__toString', '')
+                ) ?></td>
+      <td><?php echo select_tag('responsable_id[]', objects_for_select(
+                  Doctrine::getTable('sfGuardUser')->findAll(),
+                  'getId', '__toString', '')
+                ) ?></td>
     </tr>
   </tbody>
 </table>
