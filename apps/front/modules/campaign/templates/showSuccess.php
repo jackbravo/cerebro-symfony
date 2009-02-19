d1ae6b6acf1bdb827108186ae6abe6879b55569f
Elias De la Torre <elias.delatorre@axai.com.mx>
2009-02-18 19:30:33 -0600

    Agregar la tabla para que muestre cada uno de los items agregados, y que los despliegue en el showSuccess

--- apps/front/modules/campaign/templates/showSuccess.php
+++ apps/front/modules/campaign/templates/showSuccess.php
@@ -65,9 +65,62 @@
   </tbody>
 </table>
 
-<?php foreach ($campaign->Items as $item): ?>
-
-<?php endforeach; ?>
+  <table>
+    <thead>
+      <tr>
+        <th>Último acceso</th>
+        <th>Plaza</th>
+        <th>Categoria</th>
+        <th>Tipo</th>
+        <th>Responsable</th>
+        <th>Pba. color</th>
+        <th>Impresor</th>
+        <th>Entrada imp.</th>
+        <th>Salida imp.</th>
+        <th>Mensajería</th>
+        <th>No. guía</th>
+        <th>Fecha envío</th>
+        <th>Fecha recepción</th>
+        <th>Permisionario</th>
+        <th>Carrocería</th>
+        <th>Compra directa</th>
+        <th>Ruta</th>
+        <th>Placas</th>
+        <th>Económico</th>
+        <th>Instalación</th>
+        <th>Desmontaje</th>
+        <th>Evidencia</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($campaign->Items as $item): ?>
+      <tr>
+        <td title="Última actualización"><?php echo $item->fecha_actualizacion ?></td>
+        <td title="Plaza"><?php echo $item->Plaza ?></td>
+        <td title="Categoría"><?php echo $item->Categoria ?></td>
+        <td title="Tipo"><?php echo $item->Tipo ?></td>
+        <td title="Responsable"><?php echo $item->Responsable ?></td>
+        <td title="¿Existe prueba de color?"><?php echo $item->prueba_color ?></td>
+        <td title="Impresor"><?php echo $item->Impresor ?></td>
+        <td title="Entrada a impresión"><?php echo $item->entrada_impresor ?></td>
+        <td title="Salida de impresión"><?php echo $item->salida_impresor ?></td>
+        <td title="Mensajeria"><?php echo $item->Mensajeria ?></td>
+        <td title="Número de guía"><?php echo $item->guia ?></td>
+        <td title="Fecha de envío"><?php echo $item->fecha_envio ?></td>
+        <td title="Fecha de recepción"><?php echo $item->fecha_recepcion ?></td>
+        <td title="Permisionario"><?php echo $item->permisionario ?></td>
+        <td title="Carrocería"><?php echo $item->carroceria ?></td>
+        <td title="Compra directa"><?php echo $item->compra_directa ?></td>
+        <td title="Ruta"><?php echo $item->ruta ?></td>
+        <td title="Placas"><?php echo $item->placas ?></td>
+        <td title="Número económico"><?php echo $item->economico ?></td>
+        <td title="Fecha de instalación"><?php echo $item->instalacion ?></td>
+        <td title="Fecha de desmontaje"><?php echo $item->desmontaje ?></td>
+        <td title="¿Existe evidencia de la instalación?"><?php echo $item->evidencia ?></td>
+      </tr>
+      <?php endforeach; ?>
+    </tbody>   
+  </table>
 
 <a href="<?php echo url_for('campaign_edit', $campaign) ?>">Editar datos de campaña</a>
 &nbsp;
