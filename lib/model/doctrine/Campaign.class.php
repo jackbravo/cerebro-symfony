00b7f2675959f9d3649f8cf73982178d22911552
Elias De la Torre <elias.delatorre@axai.com.mx>
2009-02-22 14:39:15 -0600

    cambie el query para que devuelva las campañas del usuario involucrado

--- lib/model/doctrine/Campaign.class.php
+++ lib/model/doctrine/Campaign.class.php
@@ -9,7 +9,7 @@ class Campaign extends BaseCampaign
     $diff= (strtotime($this->fecha_cierre))-(strtotime($this->fecha_inicio));
     return $diff;
   }
-  public function getItems(){
+  public function getItems($responsable_id = null){
     $q = Doctrine_Query::create()
       ->from('Item i')
       ->leftJoin('i.Responsable r')
@@ -18,9 +18,13 @@ class Campaign extends BaseCampaign
       ->leftJoin('i.Tipo t')
       ->leftJoin('i.Impresor imp')
       ->leftJoin('i.Mensajeria m')
-      ->where('i.campaign_id=?')
+      ->addWhere('i.campaign_id=?', array($this->id))
       ->orderBy('i.responsable_id');
-    return $q->execute(array($this->id));  // $query= $this->id // es mi Id de mi campaña, lo puedo usar para el query item.campaign_id = ?
+
+    if ($responsable_id){
+      $q->addWhere('i.responsable_id = ?', array($responsable_id));
+    }
+    return $q->execute();  // $query= $this->id // es mi Id de mi campaña, lo puedo usar para el query item.campaign_id = ?
     
   }
 }
