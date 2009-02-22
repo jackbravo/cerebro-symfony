9d7a474b8f096c046ea03e1e02e93479ade4e65f
Elias De la Torre <elias.delatorre@axai.com.mx>
2009-02-22 14:37:43 -0600

    Si los campos están SET los muestro, si no no

--- apps/front/modules/item/templates/_form.php
+++ apps/front/modules/item/templates/_form.php
@@ -17,41 +17,43 @@
     </tfoot>
     <tbody>
       <?php echo $form->renderGlobalErrors() ?>
-      <tr>
-        <th><?php echo $form['campaign_id']->renderLabel() ?></th>
-        <td>
-          <?php echo $form['campaign_id']->renderError() ?>
-          <?php echo $form['campaign_id'] ?>
-        </td>
-      </tr>
-      <tr>
-        <th><?php echo $form['plaza_id']->renderLabel() ?></th>
-        <td>
-          <?php echo $form['plaza_id']->renderError() ?>
-          <?php echo $form['plaza_id'] ?>
-        </td>
-      </tr>
-      <tr>
-        <th><?php echo $form['categoria_id']->renderLabel() ?></th>
-        <td>
-          <?php echo $form['categoria_id']->renderError() ?>
-          <?php echo $form['categoria_id'] ?>
-        </td>
-      </tr>
-      <tr>
-        <th><?php echo $form['tipo_id']->renderLabel() ?></th>
-        <td>
-          <?php echo $form['tipo_id']->renderError() ?>
-          <?php echo $form['tipo_id'] ?>
-        </td>
-      </tr>
-      <tr>
-        <th><?php echo $form['responsable_id']->renderLabel() ?></th>
-        <td>
-          <?php echo $form['responsable_id']->renderError() ?>
-          <?php echo $form['responsable_id'] ?>
-        </td>
-      </tr>
+      <?php if (isset($form['plaza_id'])): ?>
+        <tr>
+          <th><?php echo $form['plaza_id']->renderLabel() ?></th>
+          <td>
+            <?php echo $form['plaza_id']->renderError() ?>
+            <?php echo $form['plaza_id'] ?>
+          </td>
+        </tr>
+      <?php endif; ?>
+      <?php if (isset($form['categoria_id'])): ?>
+        <tr>
+          <th><?php echo $form['categoria_id']->renderLabel() ?></th>
+          <td>
+            <?php echo $form['categoria_id']->renderError() ?>
+            <?php echo $form['categoria_id'] ?>
+          </td>
+        </tr>
+      <?php endif; ?>
+      <?php if (isset($form['tipo_id'])): ?>
+        <tr>
+          <th><?php echo $form['tipo_id']->renderLabel() ?></th>
+          <td>
+            <?php echo $form['tipo_id']->renderError() ?>
+            <?php echo $form['tipo_id'] ?>
+          </td>
+        </tr>
+      <?php endif; ?>
+      <?php if (isset($form['responsable_id'])): ?>
+        <tr>
+          <th><?php echo $form['responsable_id']->renderLabel() ?></th>
+          <td>
+            <?php echo $form['responsable_id']->renderError() ?>
+            <?php echo $form['responsable_id'] ?>
+          </td>
+        </tr>
+      <?php endif; ?>
+      <?php if (isset($form['prueba_color'])): ?>
       <tr>
         <th><?php echo $form['prueba_color']->renderLabel() ?></th>
         <td>
@@ -59,6 +61,8 @@
           <?php echo $form['prueba_color'] ?>
         </td>
       </tr>
+      <?php endif; ?>
+      <?php if (isset($form['impresor_id'])): ?>
       <tr>
         <th><?php echo $form['impresor_id']->renderLabel() ?></th>
         <td>
@@ -66,6 +70,8 @@
           <?php echo $form['impresor_id'] ?>
         </td>
       </tr>
+      <?php endif; ?>
+      <?php if (isset($form['entrada_impresor'])): ?>
       <tr>
         <th><?php echo $form['entrada_impresor']->renderLabel() ?></th>
         <td>
@@ -73,6 +79,8 @@
           <?php echo $form['entrada_impresor'] ?>
         </td>
       </tr>
+      <?php endif; ?>
+      <?php if (isset($form['salida_impresor'])): ?>
       <tr>
         <th><?php echo $form['salida_impresor']->renderLabel() ?></th>
         <td>
@@ -80,6 +88,8 @@
           <?php echo $form['salida_impresor'] ?>
         </td>
       </tr>
+      <?php endif; ?>
+      <?php if (isset($form['mensajeria_id'])): ?>
       <tr>
         <th><?php echo $form['mensajeria_id']->renderLabel() ?></th>
         <td>
@@ -87,6 +97,8 @@
           <?php echo $form['mensajeria_id'] ?>
         </td>
       </tr>
+      <?php endif; ?>
+      <?php if (isset($form['guia'])): ?>
       <tr>
         <th><?php echo $form['guia']->renderLabel() ?></th>
         <td>
@@ -94,6 +106,8 @@
           <?php echo $form['guia'] ?>
         </td>
       </tr>
+      <?php endif; ?>
+      <?php if (isset($form['fecha_envio'])): ?>
       <tr>
         <th><?php echo $form['fecha_envio']->renderLabel() ?></th>
         <td>
@@ -101,6 +115,8 @@
           <?php echo $form['fecha_envio'] ?>
         </td>
       </tr>
+      <?php endif; ?>
+      <?php if (isset($form['fecha_recepcion'])): ?>
       <tr>
         <th><?php echo $form['fecha_recepcion']->renderLabel() ?></th>
         <td>
@@ -108,6 +124,8 @@
           <?php echo $form['fecha_recepcion'] ?>
         </td>
       </tr>
+      <?php endif; ?>
+      <?php if (isset($form['permisionario'])): ?>
       <tr>
         <th><?php echo $form['permisionario']->renderLabel() ?></th>
         <td>
@@ -115,6 +133,8 @@
           <?php echo $form['permisionario'] ?>
         </td>
       </tr>
+      <?php endif; ?>
+      <?php if (isset($form['carroceria'])): ?>
       <tr>
         <th><?php echo $form['carroceria']->renderLabel() ?></th>
         <td>
@@ -122,6 +142,8 @@
           <?php echo $form['carroceria'] ?>
         </td>
       </tr>
+      <?php endif; ?>
+      <?php if (isset($form['compra_directa'])): ?>
       <tr>
         <th><?php echo $form['compra_directa']->renderLabel() ?></th>
         <td>
@@ -129,6 +151,8 @@
           <?php echo $form['compra_directa'] ?>
         </td>
       </tr>
+      <?php endif; ?>
+      <?php if (isset($form['ruta'])): ?>
       <tr>
         <th><?php echo $form['ruta']->renderLabel() ?></th>
         <td>
@@ -136,6 +160,8 @@
           <?php echo $form['ruta'] ?>
         </td>
       </tr>
+      <?php endif; ?>
+      <?php if (isset($form['placas'])): ?>
       <tr>
         <th><?php echo $form['placas']->renderLabel() ?></th>
         <td>
@@ -143,6 +169,8 @@
           <?php echo $form['placas'] ?>
         </td>
       </tr>
+      <?php endif; ?>
+      <?php if (isset($form['economico'])): ?>
       <tr>
         <th><?php echo $form['economico']->renderLabel() ?></th>
         <td>
@@ -150,6 +178,8 @@
           <?php echo $form['economico'] ?>
         </td>
       </tr>
+      <?php endif; ?>
+      <?php if (isset($form['instalacion'])): ?>
       <tr>
         <th><?php echo $form['instalacion']->renderLabel() ?></th>
         <td>
@@ -157,6 +187,8 @@
           <?php echo $form['instalacion'] ?>
         </td>
       </tr>
+      <?php endif; ?>
+      <?php if (isset($form['desmontaje'])): ?>
       <tr>
         <th><?php echo $form['desmontaje']->renderLabel() ?></th>
         <td>
@@ -164,6 +196,8 @@
           <?php echo $form['desmontaje'] ?>
         </td>
       </tr>
+      <?php endif; ?>
+      <?php if (isset($form['evidencia'])): ?>
       <tr>
         <th><?php echo $form['evidencia']->renderLabel() ?></th>
         <td>
@@ -171,6 +205,7 @@
           <?php echo $form['evidencia'] ?>
         </td>
       </tr>
+      <?php endif; ?>
     </tbody>
   </table>
 </form>
