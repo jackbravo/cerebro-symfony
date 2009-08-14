17a09519dd23bae9793dbf0d0ccde086e7ab522f
Joaquin Bravo Contreras <jackbravo@gmail.com>
2009-08-14 16:29:29 -0500

    fix error while saving users

--- lib/form/doctrine/sfDoctrineGuardPlugin/sfGuardUserForm.class.php
+++ lib/form/doctrine/sfDoctrineGuardPlugin/sfGuardUserForm.class.php
@@ -11,5 +11,100 @@ class sfGuardUserForm extends PluginsfGuardUserForm
 {
   public function configure()
   {
+    unset(
+      $this['last_login'],
+      $this['created_at'],
+      $this['updated_at'],
+      $this['salt'],
+      $this['algorithm']
+    );
+
+    $this->widgetSchema['groups_list']->setLabel('Groups');
+    $this->widgetSchema['permissions_list']->setLabel('Permissions');
+
+    $this->widgetSchema['password'] = new sfWidgetFormInputPassword();
+    $this->validatorSchema['password']->setOption('required', false);
+    $this->widgetSchema['password_again'] = new sfWidgetFormInputPassword();
+    $this->validatorSchema['password_again'] = clone $this->validatorSchema['password'];
+
+    $this->widgetSchema->moveField('password_again', 'after', 'password');
+
+    $this->mergePostValidator(new sfValidatorSchemaCompare('password', sfValidatorSchemaCompare::EQUAL, 'password_again', array(), array('invalid' => 'The two passwords must be the same.')));
+  }
+
+  public function savegroupsList($con = null)
+  {
+    if (!$this->isValid())
+    {
+      throw $this->getErrorSchema();
+    }
+
+    if (!isset($this->widgetSchema['groups_list']))
+    {
+      // somebody has unset this widget
+      return;
+    }
+
+    if (is_null($con))
+    {
+      $con = $this->getConnection();
+    }
+
+    $existing = $this->object->groups->getPrimaryKeys();
+    $values = $this->getValue('groups_list');
+    if (!is_array($values))
+    {
+      $values = array();
+    }
+
+    $unlink = array_diff($existing, $values);
+    if (count($unlink))
+    {
+      $this->object->unlink('groups', array_values($unlink), true);
+    }
+
+    $link = array_diff($values, $existing);
+    if (count($link))
+    {
+      $this->object->link('groups', array_values($link), true);
+    }
+  }
+
+  public function savepermissionsList($con = null)
+  {
+    if (!$this->isValid())
+    {
+      throw $this->getErrorSchema();
+    }
+
+    if (!isset($this->widgetSchema['permissions_list']))
+    {
+      // somebody has unset this widget
+      return;
+    }
+
+    if (is_null($con))
+    {
+      $con = $this->getConnection();
+    }
+
+    $existing = $this->object->permissions->getPrimaryKeys();
+    $values = $this->getValue('permissions_list');
+    if (!is_array($values))
+    {
+      $values = array();
+    }
+
+    $unlink = array_diff($existing, $values);
+    if (count($unlink))
+    {
+      $this->object->unlink('permissions', array_values($unlink), true);
+    }
+
+    $link = array_diff($values, $existing);
+    if (count($link))
+    {
+      $this->object->link('permissions', array_values($link), true);
+    }
   }
 }
